c093538c7e648b6d7e0579ea1f708f2466366f9a
Iznaen <tanggapili.iznaen@gmail.com>
2026-01-20 22:23:38 +0800

    struktur, layout, styling: header

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,30 @@ export function initHeader()
     const header = document.getElementById('header');
 
     const headerHTML = `
-    <div class="header-temp">HEADER</div>
+    <div id="headerWrapper">
+    <div class="header-left" id="headerLeft">
+        <h3>PENGOLAHAN DATA HIDROLOGI</h3>
+        <p>Unit Kerja Hidrologi dan Kualitas Air</p>
+    </div>
+
+
+    <div class="header-center" id="headerCenter">
+        <span>[dokumen_aktif]</span>
+    </div>
+
+
+    <div class="header-right" id="headerRight">
+        <button id="helpBtn" title="Bantuan">
+            <i class="fas fa-circle-question"></i>
+        </button>
+        <button id="settingsBtn" title="Pengaturan">
+            <i class="fas fa-gear"></i>
+        </button>
+        <button id="profileBtn" title="Profil">
+            <i class="fas fa-circle-user"></i>
+        </button>
+    </div>
+    </div>
     `;
 
     header.innerHTML = headerHTML;
